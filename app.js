039dded1e0cf05aea4d48f98efd8914803859dfa
Gusev <ang82v@gmail.com>
2021-04-28 08:46:29 +0300

    первое задание

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-let car = 'BMW'
-console.log(cars)
-alert(5);
-[].push
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
+
+let a = prompt('Последний фильм1', '');
+let b = +prompt('Оцените фильм1', '');
+let c = prompt('Последний фильм2', '');
+let d = +prompt('Оцените фильм2', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
